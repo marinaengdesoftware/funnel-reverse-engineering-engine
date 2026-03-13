fcde4e5ed377e6d94914f90f09fe21cdc4fdd2e8
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 14:08:00 -0300

    Update pixel_detector.py

--- detectors/pixel_detector.py
+++ detectors/pixel_detector.py
@@ -1,14 +1,22 @@
 def detect_pixels(html):
 
+    html = html.lower()
+
     pixels = []
 
-    if "connect.facebook.net" in html:
+    if "facebook.net" in html or "fbq(" in html:
         pixels.append("Facebook Pixel")
 
-    if "google-analytics.com" in html:
+    if "google-analytics.com" in html or "gtag(" in html:
         pixels.append("Google Analytics")
 
     if "googletagmanager.com" in html:
         pixels.append("Google Tag Manager")
+
+    if "clarity.ms" in html:
+        pixels.append("Microsoft Clarity")
+
+    if "tiktok" in html:
+        pixels.append("TikTok Pixel")
 
     return pixels
