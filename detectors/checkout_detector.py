3f66014eaccefdaf65c662e664bd075845a974e0
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 12:28:53 -0300

    Update checkout_detector.py

--- detectors/checkout_detector.py
+++ detectors/checkout_detector.py
@@ -1,1 +1,14 @@
+def detect_checkout(html):
 
+    checkouts = []
+
+    if "hotmart" in html:
+        checkouts.append("Hotmart")
+
+    if "kiwify" in html:
+        checkouts.append("Kiwify")
+
+    if "stripe" in html:
+        checkouts.append("Stripe")
+
+    return checkouts
