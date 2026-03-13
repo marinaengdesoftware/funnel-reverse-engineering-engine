4ae34d5c0a78b5958033d455576d22ffbe74c233
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 12:28:22 -0300

    Update automation_detector.py

--- detectors/automation_detector.py
+++ detectors/automation_detector.py
@@ -1,1 +1,14 @@
+def detect_automation(html):
 
+    tools = []
+
+    if "sleekflow" in html:
+        tools.append("Sleekflow")
+
+    if "activecampaign" in html:
+        tools.append("ActiveCampaign")
+
+    if "mailchimp" in html:
+        tools.append("Mailchimp")
+
+    return tools
