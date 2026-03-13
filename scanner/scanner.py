0e205c0c158c808c4b94d1297e87214f445dc91d
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 12:29:44 -0300

    Update scanner.py

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,35 +1,33 @@
-import requests
+from crawler.crawler import fetch_page
+
+from detectors.cms_detector import detect_cms
+from detectors.pixel_detector import detect_pixels
+from detectors.video_detector import detect_video_platform
+from detectors.automation_detector import detect_automation
+from detectors.checkout_detector import detect_checkout
+
+from report.report_generator import generate_report
+
 
 def scan_site(url):
-    response = requests.get(url)
-    html = response.text
+
+    html = fetch_page(url)
 
     results = {}
 
-    # CMS
-    if "wp-content" in html:
-        results["CMS"] = "WordPress"
-
-    # Vimeo
-    if "vimeo.com" in html:
-        results["Video Platform"] = "Vimeo"
-
-    # Facebook Pixel
-    if "facebook.net" in html:
-        results["Facebook Pixel"] = "Detected"
-
-    # Google Analytics
-    if "google-analytics.com" in html:
-        results["Google Analytics"] = "Detected"
+    results["CMS"] = detect_cms(html)
+    results["Pixels"] = detect_pixels(html)
+    results["Video Platform"] = detect_video_platform(html)
+    results["Automation Tools"] = detect_automation(html)
+    results["Checkout Platforms"] = detect_checkout(html)
 
     return results
 
 
 if __name__ == "__main__":
-    url = input("Digite a URL: ")
+
+    url = input("Digite a URL do site: ")
+
     data = scan_site(url)
 
-    print("\nFUNNEL TECH REPORT\n")
-
-    for key, value in data.items():
-        print(f"{key}: {value}")
+    generate_report(data)
