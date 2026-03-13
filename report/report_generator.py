778572681f20c8e211197606acf6234705c794fc
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 12:29:16 -0300

    Update report_generator.py

--- report/report_generator.py
+++ report/report_generator.py
@@ -1,1 +1,10 @@
+def generate_report(data):
 
+    print("\n==========================")
+    print(" FUNNEL TECH REPORT ")
+    print("==========================\n")
+
+    for key, value in data.items():
+        print(f"{key}: {value}")
+
+    print("\n==========================\n")
