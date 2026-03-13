0875f77e3aea2c45e250e5004f4174c456e32c83
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 12:28:09 -0300

    Update video_detector.py

--- detectors/video_detector.py
+++ detectors/video_detector.py
@@ -1,1 +1,14 @@
+def detect_video_platform(html):
 
+    videos = []
+
+    if "vimeo.com" in html:
+        videos.append("Vimeo")
+
+    if "youtube.com" in html:
+        videos.append("YouTube")
+
+    if "wistia" in html:
+        videos.append("Wistia")
+
+    return videos
