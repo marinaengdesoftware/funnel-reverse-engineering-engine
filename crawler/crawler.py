cf6ff5d6dbae44e33f0b1ef46d4645fd055b2a46
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 14:03:07 -0300

    Update crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,9 +1,31 @@
 import requests
 
+
 def fetch_page(url):
+
+    print("\n[LOG] Baixando página...")
+
     try:
-        response = requests.get(url, timeout=10)
-        return response.text
+
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
+
+        response = requests.get(url, headers=headers, timeout=15)
+
+        print("[LOG] Status HTTP:", response.status_code)
+
+        html = response.text
+
+        print("[LOG] Tamanho do HTML:", len(html))
+
+        if len(html) < 500:
+            print("[WARNING] HTML muito pequeno. Pode ser bloqueio ou redirecionamento.")
+
+        return html
+
     except Exception as e:
-        print("Erro ao acessar página:", e)
+
+        print("[ERRO] Falha ao acessar página:", e)
+
         return ""
