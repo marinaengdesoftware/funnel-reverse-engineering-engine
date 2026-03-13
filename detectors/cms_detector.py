7932d7406f0f8ed494e7460936fa7d0f7bd3a08c
Marina de Alcantara Andrade <127579264+marinaengdesoftware@users.noreply.github.com>
2026-03-13 12:27:38 -0300

    Update cms_detector.py

--- detectors/cms_detector.py
+++ detectors/cms_detector.py
@@ -1,1 +1,12 @@
+def detect_cms(html):
 
+    if "wp-content" in html or "wordpress" in html:
+        return "WordPress"
+
+    if "shopify" in html:
+        return "Shopify"
+
+    if "wix.com" in html:
+        return "Wix"
+
+    return "Unknown"
